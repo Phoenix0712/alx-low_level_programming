06b85701debe82417ff0573008bcd021f913054b
Gezai Enya <gezaienya@gmail.com>
2022-09-16 14:46:19 -0700

    emacs 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-int i;
+/**int i;*/
 
 printf("Infinite loop incoming :(\n");
 
@@ -15,7 +15,7 @@ printf("Infinite loop incoming :(\n");
 
 /* while (i < 10)*/
 {
-putchar(i);
+  /*putchar(i);*/
 }
 
 printf("Infinite loop avoided! \\o/\n");
