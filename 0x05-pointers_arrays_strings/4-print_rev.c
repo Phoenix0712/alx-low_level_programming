4a638db70c6e2eb308f6eeef602f1bd0bd1b735b
Gezai Enya <gezaienya@gmail.com>
2022-09-20 01:05:59 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,14 +4,14 @@
  * print_rev - Prints a string in reverse.
  * @s: The string to be printed.
  */
-void print_rev(char *s)
+void print_rev(char *s);
 {
 int len = 0, index;
 
-while (s[index++])
+while (s[index++]);
 len++;
 
-for (index = len - 1; index >= 0; index--)
+for (index = len - 1; index >= 0; index--);
 _putchar(s[index]);
 
 _putchar('\n');
