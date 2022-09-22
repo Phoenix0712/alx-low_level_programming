d7a89c85201433172277626c989fe241831ad0c8
Gezai Enya <gezaienya@gmail.com>
2022-09-22 14:30:11 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * strcat - Concatenates the string pointed to by @src, including the terminating
@@ -9,18 +10,16 @@
  * Return: A pointer to the destination string @dest.
  */
 
+
 int main()
 {
-	char *strcat(char *dest, const char *src)
+	char *_strcat(char *dest, char *src);
+ char dest[100], src[100];
+ strcpy(dest, " ");
+ strcpy(src, " ");
+ strcat(dest, src);
+ printf("%s", dest);
 
-
-	int index = 0, dest_len = 0;
-	while (dest[index++]);
-	dest_len++;
-	
-	for (index = 0; src[index]; index++);
-	dest[dest_len++] = src[index];
-	
-	return (dest);
+ return 0;
 
 }
